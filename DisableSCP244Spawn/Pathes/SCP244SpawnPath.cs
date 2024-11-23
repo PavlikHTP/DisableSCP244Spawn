aa78db86f2ec5e5dd5e3c4692b38362bfd1c44f7
pavlikHTP <kadomtsewpavlikhtp@gmail.com>
2024-11-23 15:17:15 +0500

    update

---
+++ DisableSCP244Spawn/Pathes/SCP244SpawnPath.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using InventorySystem.Items.Usables.Scp244;
+
+namespace InventorySystem.Items.Usables
+{
+    [HarmonyPatch(typeof(Scp244Spawner), nameof(Scp244Spawner.SpawnScp244))]
+    internal class Scp244Item
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ DisableSCP244Spawn/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using System;
+
+namespace CustomPlugin
+{
+    public class Plugin : Plugin<Config>
+    {
+
+        public override string Prefix => "DisableSCP244Spawn";
+
+        public override string Author => "pavlikHTP";
+
+        public override string Name => "AntiSCP244Spawn";
+
+        public override Version Version => new Version(1, 0, 0);
+
+        private Harmony _harmony;
+        private string _harmonyId;
+        public override void OnEnabled()
+        {
+
+
+            _harmonyId = $"{DateTime.Now.Ticks}";
+            _harmony = new Harmony(_harmonyId);
+            Log.Debug($"{nameof(OnEnabled)}: Patching..");
+            _harmony.PatchAll();
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+
+            Log.Info($"{Name} has been disabled!");
+            _harmony.UnpatchAll();
+            base.OnDisabled();
+        }
+
+        public override void OnReloaded()
+        {
+            base.OnReloaded();
+        }
+
+    }
+}
